a2e463b77f9550d6d191033b6c8b760435746216
AvaneeshKad <avaneeshkad@gmail.com>
2026-08-02 17:24:55 -0400

    Prepare release v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,15 @@ android {
     namespace = "com.example.localnotes"
     compileSdk = 35
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("localnotes-release.jks")
+            storePassword = "tuesday_49"
+            keyAlias = "localnotes_alias"
+            keyPassword = "tuesday_49"
+        }
+    }
+
     defaultConfig {
         applicationId = "com.example.localnotes"
         minSdk = 26
@@ -23,6 +32,7 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -60,7 +70,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material3:material3")
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.material.icons.extended)
     testImplementation(libs.junit)
